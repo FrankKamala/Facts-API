956777f48ffae6e984a8cbbec60e160d52831695
MarkWatitwa <markwatitwa98@gmail.com>
2020-03-17 13:41:43 +0300

    customized menu

--- resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
+++ resources/views/vendor/backpack/base/inc/sidebar_content.blade.php
@@ -1,6 +1,18 @@
 <!-- This file is used to store sidebar items, starting with Backpack\Base 0.9.0 -->
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('dashboard') }}"><i class="fa fa-dashboard nav-icon"></i> {{ trans('backpack::base.dashboard') }}</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('user') }}'><i class='nav-icon fa fa-question'></i> Users</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('invoice') }}'><i class='nav-icon fa fa-question'></i> Invoices</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('role') }}'><i class='nav-icon fa fa-question'></i> Roles</a></li>
-<li class='nav-item'><a class='nav-link' href='{{ backpack_url('invoicestatus') }}'><i class='nav-icon fa fa-question'></i> InvoiceStatuses</a></li>
+
+<li class="nav-item nav-dropdown">
+  <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon fa fa-group"></i> Authentication</a>
+  <ul class="nav-dropdown-items">
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('user') }}"><i class="nav-icon fa fa-user"></i> <span>Users</span></a></li>
+    <li class="nav-item"><a class="nav-link" href="{{ backpack_url('role') }}"><i class="nav-icon fa fa-group"></i> <span>Roles</span></a></li>
+  </ul>
+</li>
+
+<li class="nav-item nav-dropdown">
+  <a class="nav-link nav-dropdown-toggle" href="#"><i class="nav-icon fa fa-copy"></i> Invoice</a>
+  <ul class="nav-dropdown-items">
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('invoice') }}'><i class='nav-icon fa fa-file'></i> Invoices</a></li>
+    <li class='nav-item'><a class='nav-link' href='{{ backpack_url('invoicestatus') }}'><i class='nav-icon fa fa-question'></i> InvoiceStatuses</a></li>
+  </ul>
+</li>
