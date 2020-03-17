7bd339a7d402e46be70be6d150e059aa52b302bf
MarkWatitwa <markwatitwa98@gmail.com>
2020-03-17 21:59:56 +0300

    transaction and transaction type migrations

--- database/migrations/2020_03_17_185841_create_transaction_types_table.php
+++ database/migrations/2020_03_17_185841_create_transaction_types_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTransactionsTable extends Migration
+class CreateTransactionTypesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,9 @@ class CreateTransactionsTable extends Migration
      */
     public function up()
     {
-        Schema::create('transactions', function (Blueprint $table) {
+        Schema::create('transaction_types', function (Blueprint $table) {
             $table->id();
-            $table->bigInteger('transaction_type');
-            $table->bigInteger('account_id');
-            $table->bigInteger('invoice_id');
-            $table->double('transaction_amount');
+            $table->string('transaction_type');
             $table->timestamps();
         });
     }
@@ -30,6 +27,6 @@ class CreateTransactionsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('transactions');
+        Schema::dropIfExists('transaction_types');
     }
 }
