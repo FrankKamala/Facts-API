8d174732084038015dde6b08c0a35302a5ff714e
MarkWatitwa <markwatitwa98@gmail.com>
2020-03-20 17:28:39 +0300

    added dashboard items

--- config/backpack/base.php
+++ config/backpack/base.php
@@ -74,13 +74,13 @@ return [
 
     // Menu logo. You can replace this with an <img> tag if you have a logo.
     // 'project_logo'   => '<b>Facts</b> Africa',
-    'project_logo'   => '<img src="/images/facts_logo_final.png" height="50" width="100">',
+    'project_logo'   => '<img src="/images/facts_logo_final_white.png" height="50" width="100">',
 
     // Show / hide breadcrumbs on admin panel pages.
     'breadcrumbs' => true,
 
     // Horizontal navbar classes. Helps make the admin panel look similar to your project's design.
-    'header_class' => 'app-header bg-light border-0 navbar navbar-light',
+    'header_class' => 'app-header bg-blue border-0 navbar navbar-dark',
     // For background colors use: bg-dark, bg-primary, bg-secondary, bg-danger, bg-warning, bg-success, bg-info, bg-blue, bg-light-blue, bg-indigo, bg-purple, bg-pink, bg-red, bg-orange, bg-yellow, bg-green, bg-teal, bg-cyan, bg-white
     // For links to be visible on different background colors use: "navbar-dark", "navbar-light", "navbar-color"
 
--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -1,15 +1,49 @@
 @extends(backpack_view('blank'))
-
 @php
     $widgets['before_content'][] = [
         'type'        => 'jumbotron',
-        'heading'     => trans('backpack::base.welcome'),
-        'content'     => trans('backpack::base.use_sidebar'),
+        'heading'     => 'Supply Chain Finance Dashboard.',
+        'content'     => 'Easily manage your suppliers, buyers and transactions happening across mobile and web platforms.',
         'button_link' => backpack_url('logout'),
         'button_text' => trans('backpack::base.logout'),
+      ];
+
+
+    $widgets['before_content'][] = [
+        'type' => 'div',
+        'class' => 'row',
+        'content' => [
+              [
+        'type' => 'progress',
+        'class' => 'card text-white bg-green mb-2',
+        'value' => App\User::where('role', 2)->count(),
+        'description' => 'Registered Suppliers',
+        'progress' => App\User::where('role', 2)->count(),
+        'hint' => 'Significant growth'],
+              [
+        'type' => 'progress',
+        'class' => 'card text-white bg-blue mb-2',
+        'value' => App\User::where('role', 3)->count(),
+        'description' => 'Registered Buyers',
+        'progress' => App\User::where('role', 3)->count(),
+        'hint' => 'Significant growth'],
+              [
+        'type' => 'progress',
+        'class' => 'card text-white bg-danger mb-2',
+        'value' => App\Models\Invoice::where('invoice_status', 1)->count(),
+        'description' => 'Pending Invoices',
+        'progress' => App\Models\Invoice::where('invoice_status', 1)->count(),
+        'hint' => 'Significant growth'],
+              [
+        'type' => 'progress',
+        'class' => 'card text-white bg-success mb-2',
+        'value' => App\Models\Invoice::where('invoice_status', 2)->count(),
+        'description' => 'Approved Invoices',
+        'progress' => App\Models\Invoice::where('invoice_status', 2)->count(),
+        'hint' => 'Significant growth'],
+          ]
     ];
 @endphp
 
 @section('content')
-  <p>Your custom HTML can live here</p>
 @endsection
